982a74100df7cf08bbcb769ed2306f8db0a3ae5f
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-09-27 10:53:32 +0400

    Update tsd test for d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,10 +1,10 @@
 import { expectType, expectAssignable } from "tsd";
 import simpleCache from "./index";
 
-const cache = simpleCache<Record<string, any>>(5);
+const cache = simpleCache(5);
 const cacheItem = { greeting: "Hello World!" };
 const now = () => Date.now().toString();
 
 const cacheKey = now();
-expectType<Record<string, any>>(cache.set(cacheKey, cacheItem));
-expectAssignable<Record<string, any> | undefined>(cache.get(cacheKey));
+expectType<typeof cacheItem>(cache.set(cacheKey, cacheItem));
+expectAssignable<typeof cacheItem | undefined>(cache.get(cacheKey));
