a4c8531387ca98574665ac91ed6429018b9d791f
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-12-19 12:29:07 +0400

    improve ava test; add sideEffects: false

--- test.js
+++ test.js
@@ -2,14 +2,22 @@ import test from "ava";
 import delay from "delay";
 import simpleCache from "./dist/index.js";
 
-const CACHE_TTL = 5;
+const CACHE_TTL = 5 * 1000;
 const cache = simpleCache(CACHE_TTL);
 const now = () => Date.now().toString();
 const cacheItem = { greeting: "Hello World!" };
 
-test("can set value in cache", async (t) => {
+test("can set value in cache", (t) => {
   const key = now();
   const value = cache.set(key, cacheItem);
+
+  t.is(value, cacheItem);
+});
+
+test("can override ttl", (t) => {
+  const key = now();
+  const OVERRIDE_TTL = 3 * 1000;
+  const value = cache.set(key, cacheItem, { ttl: OVERRIDE_TTL });
 
   t.is(value, cacheItem);
 });
@@ -18,7 +26,8 @@ test("cache gets invalidated after TTL", async (t) => {
   const key = now();
   cache.set(key, cacheItem);
 
-  await delay(CACHE_TTL * 1000);
+  const BUFFER_TIME = 1000
+  await delay(CACHE_TTL + BUFFER_TIME);
 
   const value = cache.get(key);
   t.not(value, cacheItem);
@@ -28,10 +37,14 @@ test("can programmatically invalidate cache", async (t) => {
   const key = now();
   cache.set(key, cacheItem, { ttl: 2 * CACHE_TTL });
 
-  await delay(2 * 1000);
+  // delay for half the time
+  await delay(CACHE_TTL);
+
+  const value1 = cache.get(key);
+  t.is(value1, cacheItem);
 
   cache.unset(key);
 
-  const value = cache.get(key);
-  t.not(value, cacheItem);
+  const value2 = cache.get(key);
+  t.not(value2, cacheItem);
 });
